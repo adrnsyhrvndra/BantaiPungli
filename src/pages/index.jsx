b843dd3e251fa572c3ed9f74be182a6eca54977f
Adriansyah Ravindra <adrimedia.indonesia@gmail.com>
2024-04-20 19:44:45 +0700

    5.2 Fix Login Grid Image Pungli

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,17 +20,24 @@ const Login = () => {
 			<div className="grid grid-cols-12 h-screen">
 				<AudioGakSudiBayarPungli/>
 				<div className="col-span-6 bg-primary bg-pattern-money-pungli bg-blend-multiply bg-contain relative overflow-hidden">
-					<Image
-						className="absolute bottom-[51%] translate-y-1/2 left-[60%] -translate-x-1/2 scale-100"
-						src={gambarTukangPungli}
-					/>
-					<SliderQuoteLoginForm/>
+					<div className="grid grid-cols-12">
+						<div className="col-span-12">
+							<Image className="ml-auto mt-10" src={gambarTukangPungli}/>
+						</div>
+						<div className="col-span-12">
+							<SliderQuoteLoginForm/>
+						</div>
+					</div>
 				</div>
 				<div className="col-span-6 bg-background relative z-0 overflow-y-auto">
 					<Image className="absolute top-0 right-0 -z-10" src={elemenLoginForm}/>
 					<LogoForm paddingTop="pt-28"/>
 					<LoginFormContainer/>
-					<AlreadyAndForgotPassword marginTop="mt-6" text="Belum Punya Akun?" directText="Daftar Sekarang"/>
+					<AlreadyAndForgotPassword 
+						marginTop="mt-6" 
+						text="Belum Punya Akun?" 
+						directText="Daftar Sekarang"
+					/>
 					<CopyrightAuthForm/>
 				</div>
 			</div>
